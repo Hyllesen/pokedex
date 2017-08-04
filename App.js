b9d5ad203e27b0ca6457e535b1e260d998b7c823
Stefan Hyltoft <stefanhyltoft@Stefans-MacBook-Pro.local>
2017-08-04 21:26:58 +0200

    Added pocketmonk font and initial list

--- App.js
+++ App.js
@@ -1,11 +1,46 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, Image, FlatList, View } from "react-native";
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pokemons: []
+    };
+  }
+
+  componentDidMount() {
+    console.log("component did mount");
+    this.fetchPokemon();
+  }
+
+  async fetchPokemon() {
+    try {
+      const response = await fetch("https://pokeapi.co/api/v2/pokemon-form/1/");
+      const pokemon = await response.json();
+      const pokemons = this.state.pokemons;
+      pokemons.push(pokemon);
+      this.setState({ pokemons });
+      console.log(pokemon);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   render() {
+    console.log(this.state.pokemons);
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>It's down</Text>
+      <View>
+        <Text style={styles.title}>PokéDex</Text>
+        <FlatList
+          data={this.state.pokemons}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) =>
+            <Image
+              source={{ uri: item.sprites.front_default }}
+              style={{ width: 40, height: 40 }}
+            />}
+        />
       </View>
     );
   }
@@ -18,10 +53,11 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: "#F5FCFF"
   },
-  welcome: {
+  title: {
     fontSize: 20,
     textAlign: "center",
-    margin: 10
+    margin: 10,
+    fontFamily: "PocketMonk"
   },
   instructions: {
     textAlign: "center",
